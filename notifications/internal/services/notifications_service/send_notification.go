13148e2790fc0afb02402330eefcb053705e7ad1
sergeyiksanov <sergeyiksanov25@gmail.com>
2025-04-21 20:27:18 +0300

    added realisation for send notifications method

--- notifications/internal/services/notifications_service/send_notification.go
+++ notifications/internal/services/notifications_service/send_notification.go
@@ -7,4 +7,14 @@ import (
 )
 
 func (s *NotificationsService) SendNotification(ctx context.Context, notification *models.Notification, userIds ...string) {
+	for _, userId := range userIds {
+		go func(userId string) {
+			tokens, err := s.tokensRepository.GetTokensByUserId(ctx, userId)
+			if err != nil {
+				// TODO: добавить логирование или сбор метрик надо
+			}
+
+			s.noificationsRepository.SendNotification(ctx, notification, tokens...)
+		}(userId)
+	}
 }
